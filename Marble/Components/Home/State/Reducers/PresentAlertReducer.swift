eca763fa0ac138b3a6b54a7ee5526b6ac653cdac
Ritesh Pakala <ritesh@linenandsole.com>
2020-09-14 01:46:46 -0700

    crash fix

--- Marble/Components/Home/State/Reducers/PresentAlertReducer.swift
+++ Marble/Components/Home/State/Reducers/PresentAlertReducer.swift
@@ -19,7 +19,17 @@ struct PresentAlertReducer: Reducer {
         sideEffects: inout [EventBox],
         component: inout Component<ReducerState>) {
         
+        let componenToPass = component
         DispatchQueue.main.async {
+            if LSConst.Device.isIPad, let popover = event.alert.popoverPresentationController {
+                popover.sourceView = componenToPass.viewController?.view
+                
+                let midX = componenToPass.viewController?.view.frame.midX ?? UIScreen.main.bounds.width/2
+                let midY = componenToPass.viewController?.view.frame.height ?? UIScreen.main.bounds.height
+                
+                popover.sourceRect = CGRect(x: midX, y: midY, width: 0, height: 0)
+                popover.permittedArrowDirections = []
+            }
             UIApplication.topViewController()?.present(event.alert, animated: true, completion: nil)
         }
     }
